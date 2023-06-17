f134c6b8e8670789535353ad524c9bd4bbcf3c4c
Leonardo Almeida <131322465+AlmeidaLeoDev@users.noreply.github.com>
2023-06-16 23:00:05 -0300

    Update Program.cs
    Modificação: Nesse update o arquivo foi modificado para poder ser salvo em um arquivo txt

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Cadastro_de_Usuarios
 {
     internal class Program
     {
-        //struct
+        static string delimitadorInicio;
+        static string delimitadorFim;
+        static string tagNome;
+        static string tagDataDeNascimento;
+        static string tagNomeDaRua;
+        static string tagNumeroDaCasa;
 
+        //struct
         public struct DadosCadastraisStruct
         {
             public string Nome;
@@ -119,7 +127,7 @@ namespace Cadastro_de_Usuarios
             return retorno;
         }
 
-        public static void CadastraUsuario(ref List<DadosCadastraisStruct> ListaDeUsuarios)
+        public static Resultado_e CadastraUsuario(ref List<DadosCadastraisStruct> ListaDeUsuarios)
         {
             DadosCadastraisStruct CadastroUsuario;
             CadastroUsuario.Nome = "";
@@ -127,15 +135,74 @@ namespace Cadastro_de_Usuarios
             CadastroUsuario.NomeDaRua = "";
             CadastroUsuario.NumeroDaCasa = 0;
 
-            if (PegaString(ref CadastroUsuario.Nome, "Digite o nome completo ou pressione S para sair") != Resultado_e.Sucesso)
-                return;
-            if (PegaData(ref CadastroUsuario.DataDeNascimento, "Digite a sua data de nascimento no formato DD/MM/AAAA ou pressione S para sair") != Resultado_e.Sucesso)
-                return;
-            if (PegaString(ref CadastroUsuario.NomeDaRua, "Digite o nome da rua ou pressione S para sair") != Resultado_e.Sucesso)
-                return;
-            if (PegaUInt32(ref CadastroUsuario.NumeroDaCasa, "Digite o número da casa ou pressione S para sair") != Resultado_e.Sucesso)
-                return;
+            if (PegaString(ref CadastroUsuario.Nome, "Digite o nome completo ou pressione S para sair") == Resultado_e.Sair)
+                return Resultado_e.Sair;
+            if (PegaData(ref CadastroUsuario.DataDeNascimento, "Digite a sua data de nascimento no formato DD/MM/AAAA ou pressione S para sair") == Resultado_e.Sair)
+                return Resultado_e.Sair;
+            if (PegaString(ref CadastroUsuario.NomeDaRua, "Digite o nome da rua ou pressione S para sair") == Resultado_e.Sair)
+                return Resultado_e.Sair;
+            if (PegaUInt32(ref CadastroUsuario.NumeroDaCasa, "Digite o número da casa ou pressione S para sair") == Resultado_e.Sair)
+                return Resultado_e.Sair;
             ListaDeUsuarios.Add(CadastroUsuario);
+            return Resultado_e.Sucesso;
+        }
+
+        public static void GravaDados(string caminho, List<DadosCadastraisStruct> ListaDeUsuarios)
+        {
+            try
+            {
+                string conteudoArquivo = "";
+                foreach(DadosCadastraisStruct cadastro in ListaDeUsuarios)
+                {
+                    conteudoArquivo += delimitadorInicio + "\r\n";
+                    conteudoArquivo += tagNome + cadastro.Nome + "\r\n";
+                    conteudoArquivo += tagDataDeNascimento + cadastro.DataDeNascimento.ToString("dd/mm/aaaa") + "\r\n";
+                    conteudoArquivo += tagNomeDaRua + cadastro.NomeDaRua + "\r\n";
+                    conteudoArquivo += tagNumeroDaCasa + cadastro.NumeroDaCasa + "\r\n";
+                    conteudoArquivo += delimitadorFim + "\r\n";
+                }
+                File.WriteAllText(caminho, conteudoArquivo);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+            }
+        }
+
+        public static void CarregaDados(string caminho, ref List<DadosCadastraisStruct> ListaDeUsuarios)
+        {
+            try
+            {
+                if(File.Exists(caminho))
+                {
+                    string [] conteudoArquivo = File.ReadAllLines(caminho);
+                    DadosCadastraisStruct dadosCadastrais;
+                    dadosCadastrais.Nome = "";
+                    dadosCadastrais.DataDeNascimento = new DateTime();
+                    dadosCadastrais.NomeDaRua = "";
+                    dadosCadastrais.NumeroDaCasa = 0;
+
+                    foreach (string linha in conteudoArquivo)
+                    {
+                        if (linha.Contains(delimitadorInicio))
+                            continue;
+                        if (linha.Contains(delimitadorFim))
+                            ListaDeUsuarios.Add(dadosCadastrais);
+                        if (linha.Contains(tagNome))
+                            dadosCadastrais.Nome = linha.Replace(tagNome, "");
+                        if (linha.Contains(tagDataDeNascimento))
+                            dadosCadastrais.DataDeNascimento = Convert.ToDateTime(linha.Replace(tagDataDeNascimento, ""));
+                        if (linha.Contains(tagNomeDaRua))
+                            dadosCadastrais.NomeDaRua = linha.Replace(tagNomeDaRua, "");
+                        if (linha.Contains(tagNumeroDaCasa))
+                            Convert.ToUInt32(linha.Replace(tagNumeroDaCasa, ""));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("EXCEÇÃO: " + e.Message);
+            }
         }
 
         //Main
@@ -143,14 +210,26 @@ namespace Cadastro_de_Usuarios
         {
             List<DadosCadastraisStruct> ListaDeUsuarios = new List<DadosCadastraisStruct>();
             string opcao = "";
+            string caminhoArquivo = @"baseDeDados.txt";
+
+            CarregaDados(caminhoArquivo, ref ListaDeUsuarios);
+
+            delimitadorInicio = "##### INÍCIO #####";
+            delimitadorFim = "##### FIM #####";
+            tagNome = "NOME: ";
+            tagDataDeNascimento = "DATA_DE_NASCIMENTO: ";
+            tagNomeDaRua = "NOME_DA_RUA: ";
+            tagNumeroDaCasa = "NÚMERO_DA_CASA: ";
+
             do
             {
                 Console.WriteLine("Digite C para cadastrar um novo usuário ou S para encerrar o programa");
-                opcao = Console.ReadKey().KeyChar.ToString().ToLower();
+                opcao = Console.ReadKey(true).KeyChar.ToString().ToLower();
                 if (opcao == "c")
                 {
                     //Cadastrar um novo usuário
-                    CadastraUsuario(ref ListaDeUsuarios);
+                    if (CadastraUsuario(ref ListaDeUsuarios) == Resultado_e.Sucesso)
+                        GravaDados(caminhoArquivo, ListaDeUsuarios);
                 }
                 else if (opcao == "s")
                 {
